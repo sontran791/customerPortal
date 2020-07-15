fdd41343bd6cee5e4e2d89607d9c052af1976056
Jack-Tran <congsonit@gmail.com>
2020-07-15 15:57:17 -0500

    update CircleCI Maven config

--- src/main/java/com/java/customerportal/service/UserService.java
+++ src/main/java/com/java/customerportal/service/UserService.java
@@ -1,6 +1,7 @@
 package com.java.customerportal.service;
 
 import com.java.customerportal.model.User;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -13,4 +14,12 @@ public interface UserService {
     User findUserByUsername(String username);
 
     User findUserByEmail(String email);
+
+    User addNewUser(String firstName, String lastName, String username,
+                        String email, String role, boolean isNonLocked,
+                        boolean isActive, MultipartFile profileImage);
+
+    User updateUser(String currentUsername, String newFirstName, String newLastName, String newUsername,
+                        String newMail, String role, boolean isNonLocked,
+                        boolean isActive, MultipartFile profileImage);
 }
