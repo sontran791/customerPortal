4856ce9353869b8fdbc8a9eaf465c61502ed534a
Jack-Tran <congsonit@gmail.com>
2020-09-16 07:53:55 -0500

    add LocalDateTimeDeserializer

--- src/main/java/com/java/customerportal/configuration/LocalDateTimeDeserializer.java
+++ src/main/java/com/java/customerportal/configuration/LocalDateTimeDeserializer.java
@@ -1,7 +1,6 @@
 package com.java.customerportal.configuration;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import lombok.extern.slf4j.Slf4j;
